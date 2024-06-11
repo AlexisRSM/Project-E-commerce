51f0406a51dccc6abea7d458d509c9e543ae4496
AlexisRSM <alexismendes98@hotmail.com>
2024-06-11 23:36:27 +0100

    Initial Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './modules/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState();
+
+  useEffect(() =>{
+    async function fetchData() {
+      const response = await fetch("https://raw.githubusercontent.com/AlexisRSM/Project-E-commerce/master/data.json");
+      const products = await response.json();
+      console.log(products);
+      setProducts(products);
+    }
+    fetchData();
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    products && (
+      <Navbar />
+    )
+    
+    
+    
+    
+    
   )
 }
 
---
+++ src/modules/Navbar.jsx
@@ -0,0 +1,37 @@
+import './Navbar.css';
+function Navbar() {
+    return (
+        <div id='navBar'>
+            <div id='topHalf'>
+                <div id='logoWrapper'>
+                    <img id='logo' src="http://www.userlogos.org/files/logos/ArkAngel06/Amazon.png" />
+                </div>
+                <input type="text" placeholder="Give me all your money" />
+                <img id='backToSchool' src="http://blog.neurogistics.com/wp-content/uploads/2014/08/Back-To-School-Special-Banner.jpg" alt="" />
+
+            </div>{/*  */}
+
+
+
+
+            <div id='bottomHalf'>
+                <div id='depart/*  */ment'>Departments</div>
+                <div id='sections'>
+                    <div className="section">Your Amazon.com</div>
+                    <div className="section">Today's Deals</div>
+                    <div className="section">Gift Cards & Registry</div>
+                    <div className="section">Sell</div>
+                    <div className="section">Help</div>
+                </div>
+                <div id='accountStuff'>
+                    <div className="section">Your Account</div>
+                    <div className="section">Try Prime</div>
+                    <div className="section">Lists</div>
+                    <div className="section">Cart</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
