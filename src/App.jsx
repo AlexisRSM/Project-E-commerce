2889b8f2632b6e8923cae5a44ff328de2ff45904
AlexisRSM <alexismendes98@hotmail.com>
2024-06-14 00:49:00 +0100

    Visual Done 0.9

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './modules/Navbar'
+import Cards from './modules/Cards'
 
 function App() {
   const [products, setProducts] = useState();
 
-  useEffect(() =>{
+  useEffect(() => {
     async function fetchData() {
       const response = await fetch("https://raw.githubusercontent.com/AlexisRSM/Project-E-commerce/master/data.json");
       const products = await response.json();
@@ -15,17 +16,25 @@ function App() {
       setProducts(products);
     }
     fetchData();
-  },[])
+  }, [])
 
   return (
     products && (
-      <Navbar />
+      <>
+        <Navbar />
+        <div className='row my-2'>
+        {
+              products.map((product) => {
+                return <Cards product={product} />
+              })}
+        </div>
+      </>
     )
-    
-    
-    
-    
-    
+
+
+
+
+
   )
 }
 
